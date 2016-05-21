f2a65740d5f5f1e5b57678fdc84fc9d61875fe06
SvanDo001 <svandoodewaard@gmail.com>
2016-05-21 17:14:34 +0200

    Hallo message

--- maze-runner/src/maze/runner/MazeRunner.java
+++ maze-runner/src/maze/runner/MazeRunner.java
@@ -17,7 +17,7 @@ public class MazeRunner
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        System.out.println("Hallo!");
     }
     
 }
